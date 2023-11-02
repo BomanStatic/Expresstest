04f6e7d79818a6db8daf65edf8b7e2f55ca6287c
BomanStatic <boman.staticsites@gmail.com>
2023-11-02 17:31:38 +0100

    Added /screenshot

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ app.get("/", (req, res) => {
     res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
+app.get("/screenshot", async (req, res) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setViewport({ width: 1920, height: 1080 });
+
+    await page.goto(req.query.url);
+    const screenshot = await page.screenshot({ encoding: "binary" });
+
+    await browser.close();
+
+    res.setHeader("Content-Disposition", 'attachment;filename="coolstoryBRO.webp"');
+    res.setHeader("Content-Type", "image/png");
+    res.send(screenshot);
+});
+
 app.listen(process.env.PORT || 3000);
 
 module.exports = app;
